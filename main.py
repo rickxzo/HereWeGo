8a49e01d963beec651f6ab7ca394080540e0c67c
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-05-14 16:07:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -449,7 +449,7 @@ with open(LOG_FILE, "r") as f:
 
                 f"nohup sh -c 'source venv/bin/activate && {run}' > app.log 2>&1 & echo $! > app.pid\n",
 
-                "nohup sh -c 'source venv/bin/activate && python3 log_sender.py' > logger.log 2>&1 & echo $! > logger.pid"
+                "nohup sh -c 'source venv/bin/activate && python3 log_streamer.py' > logger.log 2>&1 & echo $! > logger.pid"
             ]
         }
     )
