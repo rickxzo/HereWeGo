59993f59a4849215a56090b111ae154a217873a0
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-05-20 01:45:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -421,7 +421,7 @@ import requests
 
 LOG_FILE = os.environ.get("LOG_FILE", "app.log")
 
-BACKEND_URL = "https://herewego-epkc.onrender.com/api/send-logs"
+BACKEND_URL = "https://herewego.website/api/send-logs"
 
 APP_URL = f"http://{host}:{port}"
 
