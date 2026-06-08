fd490a10e380ab9c6123d126cec2347d28550340
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-06-08 20:06:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,6 +58,15 @@ def connect_db():
         sslmode="require",
     )
 
+def connect_db2():
+    return psycopg2.connect(
+        host="ep-floral-king-apuib0ib-pooler.c-7.us-east-1.aws.neon.tech",
+        dbname="neondb",
+        user="neondb_owner",
+        password=os.getenv('NEONDB_PASS2'),
+        sslmode="require",
+    )
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
