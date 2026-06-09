645d4abe0b6d0af31c67fd6e32bc8a219206700c
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-06-09 10:26:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -790,6 +790,7 @@ async def list_functions(
 async def create_function(
     code: str,
     name: str,
+    language: str,
     user_id: str = Depends(get_current_user)
 ):
     try:
@@ -806,18 +807,97 @@ async def create_function(
     if not has_handler:
         raise Exception("lambda_handler function not found")
 
+    langs = {
+        "Python 3.10": {
+            "runtime": "python3.10",
+            "filename": "lambda.py",
+            "handler": "lambda.main"
+        },
+        "Python 3.11": {
+            "runtime": "python3.11",
+            "filename": "lambda.py",
+            "handler": "lambda.main"
+        },
+        "Python 3.12": {
+            "runtime": "python3.12",
+            "filename": "lambda.py",
+            "handler": "lambda.main"
+        },
+        "Python 3.13": {
+            "runtime": "python3.13",
+            "filename": "lambda.py",
+            "handler": "lambda.main"
+        },
+        "Python 3.14": {
+            "runtime": "python3.14",
+            "filename": "lambda.py",
+            "handler": "lambda.main"
+        },
+    
+        "Node.js 22.x": {
+            "runtime": "nodejs22.x",
+            "filename": "index.js",
+            "handler": "index.main"
+        },
+        "Node.js 24.x": {
+            "runtime": "nodejs24.x",
+            "filename": "index.js",
+            "handler": "index.main"
+        },
+    
+        "Java 11": {
+            "runtime": "java11",
+            "filename": "Main.java",
+            "handler": "Main::main"
+        },
+        "Java 17": {
+            "runtime": "java17",
+            "filename": "Main.java",
+            "handler": "Main::main"
+        },
+        "Java 21": {
+            "runtime": "java21",
+            "filename": "Main.java",
+            "handler": "Main::main"
+        },
+        "Java 25": {
+            "runtime": "java25",
+            "filename": "Main.java",
+            "handler": "Main::main"
+        },
+    
+        "Ruby 3.3": {
+            "runtime": "ruby3.3",
+            "filename": "lambda.rb",
+            "handler": "lambda.main"
+        },
+        "Ruby 3.4": {
+            "runtime": "ruby3.4",
+            "filename": "lambda.rb",
+            "handler": "lambda.main"
+        },
+        "Ruby 4.0": {
+            "runtime": "ruby4.0",
+            "filename": "lambda.rb",
+            "handler": "lambda.main"
+        },
+    
+    }
+
+    details = langs['language']
+    
     zip_buffer = io.BytesIO()
 
     with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as z:
-        z.writestr("lambda.py", code)
+        z.writestr(details["filename"], code)
 
     zip_bytes = zip_buffer.getvalue()
 
     response = lambda_client.create_function(
         FunctionName=str(user_id)+name,
-        Runtime="python3.12",
+        Runtime=details.runtime,
         Role=ROLE_ARN,
-        Handler="lambda.main",
+        Handler=details.handler,
         Code={"ZipFile": zip_bytes},
         Timeout=30,
         MemorySize=128,
@@ -869,7 +949,7 @@ async def create_function(
     conn = connect_db()
     cursor = conn.cursor()
     cursor.execute(
-        "INSERT INTO Functions VALUES (%s, %s, %s, %s, %s)", (user_id, arn, url, name, code)
+        "INSERT INTO Functions VALUES (%s, %s, %s, %s, %s, %s)", (user_id, arn, url, name, code, language)
     )
     conn.commit()
     conn.close()
