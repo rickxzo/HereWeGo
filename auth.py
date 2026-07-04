2b97ed4c8dd54ad2e4951289f7d2c97daaa3b905
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-07-04 12:49:53 +0530

    Add JWT authentication in auth.py
    Implement JWT authentication to retrieve current user.

---
+++ auth.py
@@ -0,0 +1,15 @@
+from jose import jwt
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+security = HTTPBearer()
+ 
+ALGORITHM = "HS256"
+JWT_SECRET = os.getenv("JWT_SECRET")
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=[ALGORITHM])
+        return payload.get("user_id")
+    except:
+        raise HTTPException(status_code=401, detail="Invalid token")
