26e422b00fa458a3207dfada5da6a6fd5d7d10db
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-07-04 12:40:13 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,13 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+load_dotenv()
 
+def connect_db():
+    return psycopg2.connect(
+        host="ep-floral-king-apuib0ib-pooler.c-7.us-east-1.aws.neon.tech",
+        dbname="neondb",
+        user="neondb_owner",
+        password=os.getenv('NEONDB_PASS2'),
+        sslmode="require",
+    )
