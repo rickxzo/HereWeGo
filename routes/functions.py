1acc1780577203949d5da171e10336dd95fd81cf
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-07-07 00:53:48 +0530

    Update functions.py

--- routes/functions.py
+++ routes/functions.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from auth import get_current_user
 from db import connect_db
 
@@ -6,14 +6,23 @@ router = APIRouter()
 
 
 @router.get("/api/functions")
-async def list_functions(
+def list_functions(
     user_id: str = Depends(get_current_user)
 ):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT name, url, code, language FROM Functions WHERE user_id = %s", (user_id,))
-    functions = cursor.fetchall()
-    conn.close()
-    return [
-        {'name': function[0], 'url': function[1], 'code': function[2], 'language': function[3]} for function in functions 
-    ] 
+    conn = None
+    cursor = None
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("SELECT name, url, code, language FROM Functions WHERE user_id = %s", (user_id,))
+        functions = cursor.fetchall()
+        return [
+            {'name': function[0], 'url': function[1], 'code': function[2], 'language': function[3]} for function in functions 
+        ] 
+    except Exception as e:
+        raise HTTPException(status_code=500, detail = "Function fetching error")
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
