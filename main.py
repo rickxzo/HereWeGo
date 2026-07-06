88ff2f5f2d58e8aa4d070fa8287ac7b699874822
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-07-06 18:43:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -551,30 +551,6 @@ def get_logs(
     }
 
 
-'''
-@app.get("/api/deployments")
-async def list_deployments(
-    repo_id: str,
-    user_id: str = Depends(get_current_user)
-):
-    try:
-        conn = connect_db()
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Internal server error (DB Connection in list deployments)")
-    try:
-        cursor = conn.cursor()
-        cursor.execute(
-            "SELECT id, link, status FROM Deployments WHERE repo_id = %s",
-            (repo_id,)
-        )
-        deployments = cursor.fetchall()
-        conn.close()
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Internal server error (DB Query in list deployments) - " + str(e))
-    return [{"id": d[0], "link": d[1], "status": d[2]} for d in deployments]
-'''
-
-
 
 @app.get("/api/create-ec2")
 def create_ec2():
